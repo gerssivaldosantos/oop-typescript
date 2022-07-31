90eedd3473f8f311e547328badbb4a3399c82248
gerssivaldosantos <canalandows@gmail.com>
2022-07-31 01:29:52 -0300

    refactor: rewrite with "public" key syntax in constructor

--- exercises/basic-about-class.ts
+++ exercises/basic-about-class.ts
@@ -1,17 +1,18 @@
 // First example: game character class
 
-class Character{
-    name: string = ''
-    health: number = 0
-    energy: number = 0
-    strength: number = 0
-    defense: number = 0
+class Character {
+    constructor(
+        public name: string,
+        public health: number,
+        public strength: number,
+        public defense: number,
+    ) {}
 }
 
-const kratos: Character = new Character()
-kratos.name = 'Kratos'
-kratos.health = 100
-kratos.energy = 100
-kratos.strength = 100
-kratos.defense = 100
+const kratos: Character = new Character(
+    'kratos',
+    100,
+    10,
+    10,
+)
 console.log(kratos)
